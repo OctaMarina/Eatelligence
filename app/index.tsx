63d3f7aaa52f66ce0854e1e88d3106f9d33c0047
Octa Marina <octamarina@Octas-Mac-mini.local>
2024-10-11 15:42:36 +0300

    Created onboarding screen

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import {Text, View} from "react-native";
+import {Redirect} from "expo-router";
+
+const Home = () =>{
+    return <Redirect href='./(auth)/welcome'/>
+}
+
+export default Home;
---
+++ constants/index.ts
@@ -0,0 +1,29 @@
+import onboarding1 from '@/assets/images/onboarding1.png'
+import onboarding2 from '@/assets/images/onboarding2.png'
+import onboarding3 from '@/assets/images/onboarding3.png'
+export const images={
+    onboarding1,
+    onboarding2,
+    onboarding3
+}
+
+export const onboarding = [
+    {
+        id:1,
+        title:'Lost in Screens? ',
+        description:'Unplugged helps you reconnect—with friends, loved ones, and yourself. Avoid those moments when everyone pulls out their phone without reason, and stay truly present.',
+        image:onboarding1,
+    },
+    {
+        id:2,
+        title:'Start a Phone-Free Session ',
+        description:'Challenge your group to stay present and engaged, resisting the temptation to check their phones—whether you’re with friends or spending quality time as a couple.',
+        image:onboarding2,
+    },
+    {
+        id:3,
+        title:'Find Time for Yourself ',
+        description:'Unplug and spend uninterrupted moments with yourself, free from distractions, allowing for reflection and focus.',
+        image:onboarding3,
+    }
+]
